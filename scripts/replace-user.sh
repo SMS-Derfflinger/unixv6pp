9f8aedc55f4935a35bfc9c69d6d9dcc34b897221
greatbridf <greatbridf@icloud.com>
2026-04-16 23:51:49 +0800

    chore: update script to quick find sources to process
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- scripts/replace-user.sh
+++ scripts/replace-user.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 funcname="$1"
 shift
 
-LC_ALL=C sed -i -E 's@u\.u_'"$funcname"'@User_get_'"$funcname"'()@g' "$@"
+list="$(git grep "u_$funcname" 'src/*.cpp' 'src/*.h' | awk '{print $1}' | tr -d : | sort -u | paste -s -)"
+echo "file list to change: $list"
+
+git grep 'u_'"$funcname"
+
+read yesno
+if [ "$yesno" != y ]; then
+        exit 1
+fi
+
+LC_ALL=C sed -i -E 's@u\.u_'"$funcname"'@User_get_'"$funcname"'()@g' $list
+
+git grep 'u_'"$funcname"
