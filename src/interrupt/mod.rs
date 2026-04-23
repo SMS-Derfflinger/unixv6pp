a9776b300ade4398c58ce09262f520909cafc3a1
greatbridf <greatbridf@icloud.com>
2026-04-23 22:20:12 +0800

    interrupt: remove unnecessary interrupt_entry_no_segment_switch
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -78,55 +78,6 @@ pub unsafe fn switch_to_kernel_segments() {
 }
 
 #[macro_export]
-macro_rules! interrupt_entry_no_segment_switch {
-    ($entry:ident, $handler:ident) => {
-        core::arch::global_asm!(
-            concat!(
-                ".text\n",
-                ".global ", stringify!($entry), "\n",
-                ".type ", stringify!($entry), ", @function\n",
-                stringify!($entry), ":\n",
-                "    pushl %ebp\n",
-                "    movl %esp, %ebp\n",
-                "    cld\n",
-                "    pushl %eax\n",
-                "    pushl %ebp\n",
-                "    pushl %edi\n",
-                "    pushl %esi\n",
-                "    pushl %edx\n",
-                "    pushl %ecx\n",
-                "    pushl %ebx\n",
-                "    pushl %es\n",
-                "    pushl %ds\n",
-                "    pushl %fs\n",
-                "    pushl %gs\n",
-                "    lea 0x4(%ebp), %edx\n",
-                "    pushl %edx\n",
-                "    lea 0x4(%esp), %edx\n",
-                "    pushl %edx\n",
-                "    call ", stringify!($handler), "\n",
-                "    addl $0x8, %esp\n",
-                "    popl %gs\n",
-                "    popl %fs\n",
-                "    popl %ds\n",
-                "    popl %es\n",
-                "    popl %ebx\n",
-                "    popl %ecx\n",
-                "    popl %edx\n",
-                "    popl %esi\n",
-                "    popl %edi\n",
-                "    popl %ebp\n",
-                "    popl %eax\n",
-                "    leave\n",
-                "    iret\n",
-                ".size ", stringify!($entry), ", . - ", stringify!($entry), "\n",
-            ),
-            options(att_syntax),
-        );
-    };
-}
-
-#[macro_export]
 macro_rules! interrupt_entry {
     ($entry:ident, $handler:ident) => {
         core::arch::global_asm!(
