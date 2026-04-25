SMS-Derfflinger/unixv6pp

commit 70f5573c208da3ef654839ab4a85563b2cc86a26

kernel: move utility to rust

src/kernel/utility.rs
@@ -1,3 +1,8 @@
+use eonix_mm::paging::PFN;
+
+use crate::compat::compat_flush_page_directory;
+use crate::machine::{global_user_page_table, kernel_page_table_mut, EntryFlags};
+
 const SECONDS_IN_MINUTE: u32 = 60;
 const SECONDS_IN_HOUR: u32 = 3600;
 const SECONDS_IN_DAY: u32 = 86400;
@@ -143,5 +148,83 @@ pub extern "C" fn _days_in_year(year: i32) -> u32 {
         366
     } else {
         365
+    }
+}
+
+/// 对应 C++ 的 Utility::CopySeg
+/// 通过内核页表中借用的两个 PTE（索引 256 和 257），
+/// 将 src 物理地址处的一个字节复制到 dst 物理地址处。
+fn copy_seg(src: usize, dst: usize) {
+    const BORROWED_PTE: usize = 256;
+    const KERNEL_SPACE_START: usize = 0xC0000000;
+    const PAGE_SIZE: usize = 0x1000;
+
+    let kernel_pt = kernel_page_table_mut();
+
+    // 保存原页表项
+    let ori_entry1 = kernel_pt[BORROWED_PTE].get();
+    let ori_entry2 = kernel_pt[BORROWED_PTE + 1].get();
+
+    // 将 src 和 dst 所在物理页映射到借用的 PTE
+    let flags = EntryFlags::PRESENT | EntryFlags::WRITE;
+    kernel_pt[BORROWED_PTE].set(Some(PFN::from_val(src / PAGE_SIZE)), flags);
+    kernel_pt[BORROWED_PTE + 1].set(Some(PFN::from_val(dst / PAGE_SIZE)), flags);
+
+    let addr_src = (KERNEL_SPACE_START + BORROWED_PTE * PAGE_SIZE + src % PAGE_SIZE) as *const u8;
+    let addr_dst =
+        (KERNEL_SPACE_START + (BORROWED_PTE + 1) * PAGE_SIZE + dst % PAGE_SIZE) as *mut u8;
+
+    // 刷新页表缓存
+    compat_flush_page_directory();
+
+    unsafe {
+        addr_dst.write_volatile(addr_src.read_volatile());
+    }
+
+    // 恢复原页表映射
+    kernel_pt[BORROWED_PTE].set(Some(ori_entry1.0), ori_entry1.1);
+    kernel_pt[BORROWED_PTE + 1].set(Some(ori_entry2.0), ori_entry2.1);
+    compat_flush_page_directory();
+}
+
+/// 对应 C++ 的 Utility::CopySeg2
+/// 通过用户页表的前两个 PTE（索引 0 和 1），
+/// 将 src 物理地址处的一个字节复制到 dst 物理地址处。
+fn copy_seg2(src: usize, dst: usize) {
+    const PAGE_SIZE: usize = 0x1000;
+
+    let user_pt = global_user_page_table();
+
+    // 保存原页表项（用户页表第一张的第 0 项和第 1 项）
+    let ori_entry1 = user_pt[0][0].get();
+    let ori_entry2 = user_pt[0][1].get();
+
+    // 将 src 和 dst 所在物理页映射到用户页表前两项
+    let flags = EntryFlags::PRESENT | EntryFlags::WRITE | EntryFlags::USER;
+    user_pt[0][0].set(Some(PFN::from_val(src / PAGE_SIZE)), flags);
+    user_pt[0][1].set(Some(PFN::from_val(dst / PAGE_SIZE)), flags);
+
+    let addr_src = (src % PAGE_SIZE) as *const u8;
+    let addr_dst = (PAGE_SIZE + dst % PAGE_SIZE) as *mut u8;
+
+    // 刷新页表缓存
+    compat_flush_page_directory();
+
+    unsafe {
+        addr_dst.write_volatile(addr_src.read_volatile());
+    }
+
+    // 恢复原页表映射
+    user_pt[0][0].set(Some(ori_entry1.0), ori_entry1.1);
+    user_pt[0][1].set(Some(ori_entry2.0), ori_entry2.1);
+    compat_flush_page_directory();
+}
+
+/// 对应 C++ 的 phys_copy
+/// 逐字节将物理地址 from 处的 len 个字节复制到物理地址 to 处。
+#[no_mangle]
+pub extern "C" fn phys_copy(from: usize, to: usize, len: usize) {
+    for i in 0..len {
+        copy_seg(from + i, to + i);
     }
 }

src/machine/mod.rs
@@ -9,7 +9,8 @@ use core::ptr::NonNull;
 use crate::sync::SuperCell;
 
 pub use page_table::{
-    global_user_page_table, switch_user_struct, EntryFlags, PageTable, PageTableEntry,
+    global_user_page_table, kernel_page_table_mut, switch_user_struct, EntryFlags, PageTable,
+    PageTableEntry,
 };
 
 const DESCRIPTOR_COUNT: usize = 256;

src/kernel/Utility.cpp
@@ -54,69 +54,7 @@ int Utility::StringLength(char* pString)
 	/* 返回字符串长度 */
 	return length;
 }
-#endif 
-
-void Utility::CopySeg2(unsigned long src, unsigned long des)
-{
-	PageTableEntry* userPageTable = (PageTableEntry*)Machine::Instance().GetUserPageTableArray();
-	
-
-	/*
-	 * 先保存原用户态第一页与第二页PageTableEntry，因为下面的操作
-	 * 将会将src所在页映射到0#目录表项，des映射到1#表项，最后进行copy
-	 */
-	unsigned long oriEntry1 = userPageTable[0].m_PageBaseAddress;
-	unsigned long oriEntry2 = userPageTable[1].m_PageBaseAddress;	
-
-	userPageTable[0].m_PageBaseAddress = src / PAGE_SIZE;
-	userPageTable[1].m_PageBaseAddress = des / PAGE_SIZE;
-
-	unsigned char* addressSrc = (unsigned char*)(src % PAGE_SIZE);	
-	//第二页virtual addess从4096开始
-	unsigned char* addressDes = (unsigned char*)(PAGE_SIZE + des % PAGE_SIZE);	
-	//需要刷新页表缓存
-	FlushPageDirectory();
-
-	*addressDes = *addressSrc;
-	
-	//恢复原页表映射
-	userPageTable[0].m_PageBaseAddress = oriEntry1;
-	userPageTable[1].m_PageBaseAddress = oriEntry2;
-	FlushPageDirectory();
-}
-
-void Utility::CopySeg(unsigned long src, unsigned long des)
-{
-	PageTableEntry* PageTable = Machine::Instance().GetKernelPageTable().m_Entrys;
-
-	/*
-	 * 先保存原用户态第一页与第二页PageTableEntry，因为下面的操作
-	 * 将会将src所在页映射到0#目录表项，des映射到1#表项，最后进行copy
-	 */
-	unsigned long oriEntry1 = PageTable[borrowedPTE].m_PageBaseAddress;
-	unsigned long oriEntry2 = PageTable[borrowedPTE + 1].m_PageBaseAddress;
-
-	PageTable[256].m_PageBaseAddress = src / PAGE_SIZE;
-	PageTable[257].m_PageBaseAddress = des / PAGE_SIZE;
-
-	unsigned char* addressSrc = (unsigned char*)(0xC0000000 + borrowedPTE * PAGE_SIZE + src % PAGE_SIZE);
-
-	unsigned char* addressDes = (unsigned char*)(0xC0000000 + (borrowedPTE + 1) * PAGE_SIZE + des % PAGE_SIZE);
-	//需要刷新页表缓存
-	FlushPageDirectory();
-
-	*addressDes = *addressSrc;
-
-	//恢复原页表映射
-	PageTable[borrowedPTE].m_PageBaseAddress = oriEntry1;
-	PageTable[(borrowedPTE + 1)].m_PageBaseAddress = oriEntry2;
-	FlushPageDirectory();
-}
-
-extern "C" void phys_copy(unsigned long from, unsigned long to, unsigned long len) {
-        while (len--)
-                Utility::CopySeg(from++, to++);
-}
+#endif
 
 short Utility::GetMajor(const short dev)
 {

src/machine/page_table.rs
@@ -210,7 +210,7 @@ fn page_directory_mut() -> &'static mut PageDirectory {
     }
 }
 
-fn kernel_page_table_mut() -> &'static mut PageTable {
+pub fn kernel_page_table_mut() -> &'static mut PageTable {
     unsafe {
         &mut *phys_to_virt(PAddr::from(KERNEL_PAGE_TABLE_BASE_ADDRESS)).cast()
     }

commit 9212383fe068565a746299929a9432253495b29b

util: disable IRQs when borrowing PTE to do paddr copies
Fixes: 70f5573 ("kernel: move utility to rust")
Signed-off-by: greatbridf <greatbridf@icloud.com>

src/compat/mod.rs
@@ -1,7 +1,6 @@
 use core::{ffi::CStr, num::NonZero};
 
 use alloc::boxed::Box;
-use eonix_mm::address::{Addr, PAddr};
 use kernel_macros::define_class_compat;
 
 use crate::{dev::buffer::PhysicalBlock, mm::SWAPPER_AREAS, sync::SpinExt, user::Userspace};
@@ -17,9 +16,7 @@ pub fn compat_flush_page_directory() {
 }
 
 pub fn compat_user_pt() -> &'static mut [usize; 2048] {
-    unsafe {
-        &mut *(0xc0202000 as *mut [usize; 2048])
-    }
+    unsafe { &mut *(0xc0202000 as *mut [usize; 2048]) }
 }
 
 pub fn compat_get_time() -> u32 {
@@ -27,9 +24,7 @@ pub fn compat_get_time() -> u32 {
         fn get_time() -> u32;
     }
 
-    unsafe {
-        get_time()
-    }
+    unsafe { get_time() }
 }
 
 define_class_compat! {impl Utils{
@@ -64,23 +59,14 @@ define_class_compat! {impl Utils{
     }
 }}
 
-pub fn compat_phys_copy(from: PAddr, to: PAddr, len: usize) {
-    extern "C" {
-        fn phys_copy(from: usize, to: usize, len: usize);
-    }
-
-    unsafe {
-        phys_copy(from.addr(), to.addr(), len);
-    }
-}
-
 const SECTOR_SIZE: usize = 512;
 
 pub fn compat_swap_alloc(bytes: usize) -> PhysicalBlock {
     let sectors = (bytes + SECTOR_SIZE - 1) / SECTOR_SIZE;
     assert_ne!(sectors, 0);
 
-    let block = SWAPPER_AREAS.lock()
+    let block = SWAPPER_AREAS
+        .lock()
         .alloc(NonZero::new(sectors).expect("Alloc 0 swap blocks"))
         .expect("Out of swap space");
 
@@ -88,15 +74,14 @@ pub fn compat_swap_alloc(bytes: usize) -> PhysicalBlock {
 }
 
 pub fn compat_swap_free(blkno: PhysicalBlock, bytes: usize) {
-    let start_blk = NonZero::new(blkno.0 as usize)
-        .expect("Free swap block 0");
-    let sectors = NonZero::new((bytes + SECTOR_SIZE - 1) / SECTOR_SIZE)
-        .expect("Free 0 swap blocks");
+    let start_blk = NonZero::new(blkno.0 as usize).expect("Free swap block 0");
+    let sectors =
+        NonZero::new((bytes + SECTOR_SIZE - 1) / SECTOR_SIZE).expect("Free 0 swap blocks");
 
-    SWAPPER_AREAS.lock().free(start_blk,sectors);
+    SWAPPER_AREAS.lock().free(start_blk, sectors);
 }
 
-define_class_compat!{impl Utility {
+define_class_compat! {impl Utility {
     pub fn panic(msg: *const i8) -> ! {
         crate::println_fatal!("Love from C++: {:?}", unsafe { CStr::from_ptr(msg) });
         panic!();

src/kernel/mod.rs
@@ -3,4 +3,4 @@ mod init;
 mod kernel;
 mod splash;
 mod syscall;
-mod utility;
+pub mod utility;

src/kernel/utility.rs
@@ -1,8 +1,10 @@
+use eonix_mm::address::{Addr, PAddr};
 use eonix_mm::paging::PFN;
 
 use crate::compat::compat_flush_page_directory;
 use crate::machine::chip::SystemTime;
 use crate::machine::{global_user_page_table, kernel_page_table_mut, EntryFlags};
+use crate::sync::IrqGuard;
 
 const SECONDS_IN_MINUTE: u32 = 60;
 const SECONDS_IN_HOUR: u32 = 3600;
@@ -212,9 +214,9 @@ fn copy_seg2(src: usize, dst: usize) {
 
 /// 对应 C++ 的 phys_copy
 /// 逐字节将物理地址 from 处的 len 个字节复制到物理地址 to 处。
-#[no_mangle]
-pub extern "C" fn phys_copy(from: usize, to: usize, len: usize) {
+pub fn phys_copy(from: PAddr, to: PAddr, len: usize) {
+    let _ctx = IrqGuard::disable_save();
     for i in 0..len {
-        copy_seg(from + i, to + i);
+        copy_seg(from.addr() + i, to.addr() + i);
     }
 }

src/proc/manager.rs
@@ -11,11 +11,12 @@ use eonix_mm::address::{Addr, PAddr};
 use eonix_sync_base::LazyLock;
 
 use crate::{
-    compat::{compat_phys_copy, compat_swap_alloc},
+    compat::compat_swap_alloc,
     constants::{PosixError, Signal},
     dev::{buffer::BufFlag, buffer_manager::global_buffer_manager},
     fs::{DirSearchMode, FileManager, InodeMode, InodeRefExt},
     interrupt::Registers,
+    kernel::utility::phys_copy,
     loader::PEParser,
     machine::{set_tss_esp0, switch_user_struct, TrapFrame},
     mm::{PAGE_SIZE, USER_PAGE_MANAGER},
@@ -137,7 +138,7 @@ impl ProcessManager {
             child.addr = pages.phys().addr();
             while cnt != 0 {
                 cnt -= 1;
-                compat_phys_copy(cur_addr, to_addr, 1);
+                phys_copy(cur_addr, to_addr, 1);
                 cur_addr = cur_addr + 1;
                 to_addr = to_addr + 1;
             }

src/proc/process.rs
@@ -11,10 +11,11 @@ use eonix_mm::{
 };
 
 use crate::{
-    compat::{compat_phys_copy, compat_swap_alloc, compat_swap_free},
+    compat::{compat_swap_alloc, compat_swap_free},
     constants::Signal,
     dev::buffer::PhysicalBlock,
     fs::{InodeRef, OpenFiles},
+    kernel::utility::phys_copy,
     machine::TrapFrame,
     mm::{PhysPage, KERNEL_PAGE_MANAGER, PAGE_SIZE, USER_PAGE_MANAGER},
     proc::{
@@ -487,7 +488,7 @@ impl Process {
         self.addr = new_addr.addr();
 
         let copylen = oldlen.min(newlen);
-        compat_phys_copy(PAddr::from_val(old_addr), new_addr, copylen);
+        phys_copy(PAddr::from_val(old_addr), new_addr, copylen);
 
         if let Some(pages) = self.pages.replace(new_page) {
             unsafe {
@@ -513,7 +514,7 @@ impl Process {
         while cnt != 0 {
             cnt -= 1;
             dst = dst - 1;
-            compat_phys_copy(dst - change, dst, 1);
+            phys_copy(dst - change, dst, 1);
         }
 
         mem.map_to_actual_pt(self);
@@ -545,7 +546,7 @@ impl Process {
 
             while cnt != 0 {
                 cnt -= 1;
-                compat_phys_copy(dst + change_abs, dst, 1);
+                phys_copy(dst + change_abs, dst, 1);
                 dst = dst + 1;
             }
             self.expand(newlen);
@@ -557,7 +558,7 @@ impl Process {
             while cnt != 0 {
                 cnt -= 1;
                 dst = dst - 1;
-                compat_phys_copy(dst - change as usize, dst, 1);
+                phys_copy(dst - change as usize, dst, 1);
             }
         }
 