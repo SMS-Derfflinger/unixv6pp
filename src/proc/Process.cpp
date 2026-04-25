770c62d0b305c5a42194d0b32550d8dae4294a8d
greatbridf <greatbridf@icloud.com>
2026-04-25 22:41:12 +0800

    cleanup: remove process/
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- src/proc/Process.cpp
+++
@@ -1,9 +0,0 @@
-#include "Process.h"
-
-extern "C" {
-	void Process_sleep_kernel(Process*, unsigned long, int);
-}
-
-void Process::Sleep(unsigned long chan, int pri) {
-	Process_sleep_kernel(this, chan, pri);
-}
