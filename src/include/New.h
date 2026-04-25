91a627eb08aa392164260b92d8ffc90b7c9f9117
greatbridf <greatbridf@icloud.com>
2026-04-25 23:56:22 +0800

    cleanup: remove C++ headers
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- src/include/New.h
+++
@@ -1,9 +0,0 @@
-#ifndef NEW_H
-#define NEW_H
-
-void* operator new(unsigned int size);
-void* operator new(__SIZE_TYPE__, void* ptr);
-void operator delete(void* p);
-void operator delete(void* p, unsigned int n);
-
-#endif
