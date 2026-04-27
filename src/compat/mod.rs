SMS-Derfflinger/unixv6pp

commit cf4e87bf62dcde8389127efe8865df3233b3db39

interrupt: move time interrupt to rust
(cherry picked from commit bf63aa79ac462dcd0a49586eda818cd1dfc94bf2)
Signed-off-by: Bingfang Guo <bingfangguo@tencent.com>

src/interrupt/mod.rs
@@ -1,9 +1,13 @@
 mod exception;
 mod interrupt;
+mod time;
 
 use core::arch::asm;
 
 use crate::dev::io_port::IOPort;
+
+pub use interrupt::send_master_eoi;
+pub use time::set_time;
 
 pub const KERNEL_MODE: u32 = 0x0;
 pub const USER_MODE: u32 = 0x3;
@@ -58,12 +62,6 @@ impl PteContext {
 
     pub fn from_user_mode(&self) -> bool {
         self.xcs & USER_MODE == USER_MODE
-    }
-}
-
-pub fn send_master_eoi() {
-    unsafe {
-        IOPort::out_byte(PIC_MASTER_IO_PORT_1, PIC_EOI);
     }
 }
 

src/interrupt/time.rs
@@ -1,0 +1,115 @@
+use crate::{
+    interrupt::{PtContext, PtRegs, interrupt::schedule_on_user_return, send_master_eoi},
+    interrupt_entry,
+    machine::asm::enable_interrupts,
+    proc::{ProcessManager, ProcessState},
+    user::Userspace,
+};
+
+const HZ: i32 = 60 * 2;
+
+static mut LBOLT: i32 = 0;
+static mut TIME: u32 = 0;
+static mut TOUT: u32 = 0;
+
+#[no_mangle]
+pub extern "C" fn time_interrupt_body(_regs: *mut PtRegs, context: *mut PtContext) {
+    clock(context);
+    schedule_on_user_return(context);
+}
+
+fn clock(context: *mut PtContext) {
+    let Some(context_ref) = (unsafe { context.as_ref() }) else {
+        send_master_eoi();
+        return;
+    };
+
+    let from_user_mode = context_ref.from_user_mode();
+    let current_status = {
+        let current = Userspace::get().proc();
+        if from_user_mode {
+            Userspace::get().utime += 1;
+        } else {
+            Userspace::get().stime += 1;
+        }
+
+        current.cpu = (current.cpu + 1).min(1024);
+
+        current.stat
+    };
+
+    unsafe {
+        LBOLT += 1;
+        if LBOLT < HZ {
+            send_master_eoi();
+            return;
+        }
+    }
+
+    if current_status == ProcessState::SRUN && !from_user_mode {
+        send_master_eoi();
+        return;
+    }
+
+    unsafe {
+        LBOLT -= HZ;
+        TIME += 1;
+    }
+
+    enable_interrupts();
+    send_master_eoi();
+
+    if get_time() == time_tout() {
+        ProcessManager::get().wakeup_all(time_tout_address());
+    }
+
+    ProcessManager::get().recalc_pri();
+
+    if ProcessManager::get().run_in != 0 {
+        ProcessManager::get().run_in = 0;
+        ProcessManager::get().wakeup_runin();
+    }
+
+    if from_user_mode {
+        let current = Userspace::get().proc();
+        if current.should_process() {
+            current.process_signal(unsafe { &mut *(context as *mut _) });
+        }
+        current.set_pri();
+    }
+}
+
+pub fn set_time(value: u32) {
+    unsafe {
+        TIME = value;
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn get_time() -> u32 {
+    unsafe { TIME }
+}
+
+#[no_mangle]
+pub extern "C" fn time_set(value: u32) {
+    set_time(value);
+}
+
+#[no_mangle]
+pub extern "C" fn time_tout() -> u32 {
+    unsafe { TOUT }
+}
+
+#[no_mangle]
+pub extern "C" fn time_set_tout(value: u32) {
+    unsafe {
+        TOUT = value;
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn time_tout_address() -> usize {
+    core::ptr::addr_of!(TOUT) as usize
+}
+
+interrupt_entry!(TimeInterruptEntrance, time_interrupt_body);

src/machine/mod.rs
@@ -283,7 +283,6 @@ unsafe extern "C" {
     safe fn KeyboardInterruptEntrance();
     safe fn DiskInterruptEntrance();
     safe fn MasterIRQ7();
-    #[link_name = "_ZN4Time21TimeInterruptEntranceEv"]
     safe fn TimeInterruptEntrance();
     #[link_name = "_ZN10SystemCall18SystemCallEntranceEv"]
     safe fn SystemCallEntrance();

src/proc/mod.rs
@@ -2,7 +2,7 @@ mod context;
 mod manager;
 mod process;
 
-pub use process::{Text, Process};
+pub use process::{Text, Process, ProcessState};
 pub use manager::ProcessManager;
 
 /// A channel that sleepers can subscribe to.

src/include/TimeInterrupt.h
@@ -1,29 +1,0 @@
-#ifndef TIME_INTERRUPT_H
-#define TIME_INTERRUPT_H
-
-class Time
-{
-	/* 
-	 * 注：由于Bochs的时钟中断不准确，一秒内时钟中断次数远大于60次，
-	 * 为了尽可能减小系统时间的误差，常数SCHMAG和HZ的值同比扩大。
-	 * 以下取值经过调试发现在Bochs下比较理想，请在了解影响范围后再进行改动！！。
-	 */
-public:
-	static const int SCHMAG = 10 * 2;	/* 每秒钟减少的p_cpu魔数 */
-
-	static const int HZ = 60 * 2;		/* 每秒钟时钟中断次数 */
-
-	static int lbolt;				/* 累计接收到的时钟中断次数 */
-	
-	static unsigned int time;		/* 系统全局时间，自1970年1月1日至今的秒数 */
-
-	static unsigned int tout;		/* 各延时睡眠进程中应被唤醒的时刻中最小值 */
-
-	/* 时钟中断入口函数，其地址存放在IDT的磁盘中断对应中断门中 */
-	static void TimeInterruptEntrance();
-
-	/* 时钟中断处理函数，维护系统时钟日历，计算进程占用CPU时间，处理睡眠进程和延时队列等工作 */
-	static void Clock(struct pt_regs* regs, struct pt_context* context);
-};
-
-#endif

src/interrupt/Makefile
@@ -1,6 +1,6 @@
 include-path := $(abs_srctree)/src $(abs_srctree)/src/include $(abs_srctree)/fs
 
-objs := SystemCall.o TimeInterrupt.o
+objs := SystemCall.o
 
 include $(abs_srctree)/scripts/Makefile
 

src/interrupt/SystemCall.cpp
@@ -2,13 +2,17 @@
 #include "User.h"
 #include "Kernel.h"
 #include "Regs.h"
-#include "TimeInterrupt.h"
 #include "Video.h"
 
 extern "C" {
 unsigned int _diagnose_rows();
 void _diagnose_enable_rows(unsigned int rows);
 void _diagnose_disable_rows();
+unsigned int get_time();
+void time_set(unsigned int value);
+unsigned int time_tout();
+void time_set_tout(unsigned int value);
+unsigned long time_tout_address();
 }
 
 
@@ -369,7 +373,7 @@ int SystemCall::Sys_ChDir()
 int SystemCall::Sys_GTime()
 {
 	User& u = Kernel::Instance().GetUser();
-	User_get_ar0()[User::EAX] = Time::time;
+	User_get_ar0()[User::EAX] = get_time();
 
 	return 0;	/* GCC likes it ! */
 }
@@ -490,7 +494,7 @@ int SystemCall::Sys_Stime()
 	/* 仅超级用户才具有设置系统时间的权限 */
 	if (Userspace_is_root())
 	{
-		Time::time = User_get_ar0()[User::EAX];
+		time_set(User_get_ar0()[User::EAX]);
 	}
 
 	return 0;	/* GCC likes it ! */
@@ -594,7 +598,7 @@ int SystemCall::Sys_Sslep()
 
 	X86Assembly::CLI();
 
-	unsigned int wakeTime = Time::time + User_get_arg()[0];	/* sleep(second) */
+	unsigned int wakeTime = get_time() + User_get_arg()[0];	/* sleep(second) */
 
 	/*
 	 * 对   if ( Time::tout <= Time::time || Time::tout > wakeTime )  中判断条件的解释：
@@ -614,13 +618,15 @@ int SystemCall::Sys_Sslep()
 	 *
 	 * 现在的闹钟服务正确，执行sleep(0)的进程不会入睡更不会使tout值出现错误。
 	 */
-	while( wakeTime > Time::time )
+	while( wakeTime > get_time() )
 	{
-		if ( Time::tout <= Time::time || Time::tout > wakeTime )
+		unsigned int now = get_time();
+		unsigned int tout = time_tout();
+		if ( tout <= now || tout > wakeTime )
 		{
-			Time::tout = wakeTime;
+			time_set_tout(wakeTime);
 		}
-		User_get_procp()->Sleep((unsigned long)&Time::tout, ProcessManager::PSLEP);
+		User_get_procp()->Sleep(time_tout_address(), ProcessManager::PSLEP);
 	}
 
 	X86Assembly::STI();

commit b3556a92e93f5e124481b44f8ee5ff5b206f6a8c

interrupt: fix timer wakeup and remove redundant codes
Fixes: cf4e87b ("interrupt: move time interrupt to rust")
Signed-off-by: greatbridf <greatbridf@icloud.com>

Cargo.toml
@@ -30,3 +30,4 @@ bitflags = "2.11.0"
 [features]
 default = []
 debug_irq = []
+debug_timer = []

src/compat/mod.rs
@@ -16,14 +16,6 @@ pub fn compat_user_pt() -> &'static mut [usize; 2048] {
     unsafe { &mut *(0xc0202000 as *mut [usize; 2048]) }
 }
 
-pub fn compat_get_time() -> u32 {
-    extern "C" {
-        fn get_time() -> u32;
-    }
-
-    unsafe { get_time() }
-}
-
 const SECTOR_SIZE: usize = 512;
 
 pub fn compat_swap_alloc(bytes: usize) -> PhysicalBlock {

src/fs/file_manager.rs
@@ -1,7 +1,6 @@
 use core::{cmp::min, ffi::CStr, ptr};
 
 use crate::{
-    compat::compat_get_time,
     constants::{PosixError, Signal},
     dev::{
         buffer::{Buffer, DevId, LogicalBlock, PhysicalBlock},
@@ -15,6 +14,7 @@ use crate::{
         inode::{InodeFlag, InodeMode},
         File, FileRef, Inode, InodeRef, InodeRefGuard,
     },
+    interrupt::get_time,
     proc::{Channel, ProcessManager},
     sync::{IrqGuard, SpinExt},
     user::Userspace,
@@ -508,7 +508,7 @@ impl FileManager {
         let inode = pinode.lock();
         let ino = inode.i_number;
         let dev = inode.i_dev;
-        inode.i_update(compat_get_time() as i32);
+        inode.i_update(get_time() as u32);
         drop(inode);
 
         let sector = FileSystem::INODE_ZONE_START_SECTOR as u32

src/fs/file_system.rs
@@ -3,7 +3,6 @@ use core::{array, ptr};
 use eonix_spin::Spin;
 
 use crate::{
-    compat::compat_get_time,
     constants::PosixError,
     dev::{
         buffer::{Buf, Buffer, DevId, PhysicalBlock},
@@ -15,6 +14,7 @@ use crate::{
         inode::{DiskInode, Inode},
         InodeRef, SuperBlockRef,
     },
+    interrupt::get_time,
     proc::{ProcessManager, PINOD},
     sync::{IrqGuard, SpinExt},
     user::Userspace,
@@ -46,7 +46,7 @@ struct DiskSuperBlock {
     pub s_ilock: i32,
     pub s_fmod: i32,
     pub s_ronly: i32,
-    pub s_time: i32,
+    pub s_time: u32,
 
     padding: [i32; 47],
 }
@@ -87,7 +87,7 @@ pub struct SuperBlock {
 }
 
 impl SuperBlock {
-    fn from_disk(mut disk: DiskSuperBlock, time: i32) -> Self {
+    fn from_disk(mut disk: DiskSuperBlock, time: u32) -> Self {
         let readonly = disk.s_ronly != 0;
         let modified = disk.s_fmod != 0;
 
@@ -133,7 +133,7 @@ impl SuperBlock {
         self.modified = modified;
     }
 
-    fn set_time(&mut self, time: i32) {
+    fn set_time(&mut self, time: u32) {
         self.disk.s_time = time;
     }
 
@@ -196,7 +196,7 @@ impl FileSystem {
     pub const INODE_NUMBER_PER_SECTOR: usize = 8;
     pub const INODE_ZONE_START_SECTOR: usize = 514;
 
-    fn install_loaded_super_block(&mut self, loaded_super_block: DiskSuperBlock, time: i32) {
+    fn install_loaded_super_block(&mut self, loaded_super_block: DiskSuperBlock, time: u32) {
         let super_block = SuperBlock::from_disk(loaded_super_block, time);
         self.m_mount[0].m_dev = DevId(ROOTDEV);
         self.m_mount[0].m_spb = Some(Arc::new(Spin::new(super_block)));
@@ -314,7 +314,7 @@ impl FileSystem {
     }
 
     pub fn load_super_block(&mut self) -> Result<(), FileSystemError> {
-        let time = compat_get_time() as i32;
+        let time = get_time() as u32;
         self.install_loaded_super_block(Self::read_super_block()?, time);
 
         Ok(())
@@ -348,7 +348,7 @@ impl FileSystem {
         }
 
         self.updlock = true;
-        let time = compat_get_time() as i32;
+        let time = get_time() as u32;
 
         for mount in &self.m_mount {
             let Some(spb) = mount.m_spb.as_ref() else {

src/fs/inode.rs
@@ -390,7 +390,7 @@ impl Inode {
         }
     }
 
-    pub fn i_update(&self, time: i32) {
+    pub fn i_update(&self, time: u32) {
         if !self.i_flag.intersects(InodeFlag::IUPD | InodeFlag::IACC) {
             return;
         }
@@ -578,6 +578,6 @@ pub struct DiskInode {
     pub d_gid: u16,
     pub d_size: u32,
     pub d_addr: [PhysicalBlock; 10],
-    pub d_atime: i32,
-    pub d_mtime: i32,
+    pub d_atime: u32,
+    pub d_mtime: u32,
 }

src/fs/open_file_manager.rs
@@ -2,7 +2,6 @@ use eonix_spin::Spin;
 use eonix_sync_base::LazyLock;
 
 use crate::{
-    compat::compat_get_time,
     constants::PosixError,
     dev::{
         buffer::{Buffer, DevId, PhysicalBlock},
@@ -15,6 +14,7 @@ use crate::{
         inode::{DiskInode, Inode, InodeFlag, InodeMode},
         FileRef, FileSlot, InodeRef, InodeRefGuard, InodeSlot,
     },
+    interrupt::get_time,
     proc::{Channel, ProcessManager, PINOD},
     sync::{IrqGuard, SpinExt},
     user::Userspace,
@@ -201,7 +201,7 @@ impl InodeTable {
             let _ = fs::global_file_system().i_free(inode.i_dev, inode.i_number);
         }
 
-        inode.i_update(compat_get_time() as i32);
+        inode.i_update(get_time() as u32);
         inode.prele();
         inode.i_flag = InodeFlag::empty();
         inode.i_number = -1;
@@ -215,7 +215,7 @@ impl InodeTable {
             }
 
             inode.i_flag |= InodeFlag::ILOCK;
-            inode.i_update(compat_get_time() as i32);
+            inode.i_update(get_time() as u32);
             inode.prele();
         }
     }

src/interrupt/mod.rs
@@ -4,7 +4,7 @@ mod system_call;
 mod time;
 
 pub use interrupt::send_master_eoi;
-pub use time::set_time;
+pub use time::{get_time, set_time};
 
 pub const KERNEL_MODE: u32 = 0x0;
 pub const USER_MODE: u32 = 0x3;

src/interrupt/system_call.rs
@@ -3,7 +3,8 @@ use core::ptr::NonNull;
 use crate::{
     constants::{PosixError, Signal, PSLEP},
     interrupt::{
-        time::{get_time, time_set_tout, time_tout, time_tout_address},
+        set_time,
+        time::{get_time, sleep_user_until},
         Registers,
     },
     interrupt_entry,
@@ -14,7 +15,6 @@ use crate::{
     machine::{asm::disable_interrupts, TrapFrame},
     proc::{ProcessManager, TaskContext},
     serial::KResult,
-    sync::IrqGuard,
     user::{Pointer, Userspace},
 };
 
@@ -208,13 +208,12 @@ fn handle_in_rust(number: usize) -> KResult<usize> {
         }
         sys::GETUID => Ok(Userspace::get().getuid() as usize),
         sys::STIME => {
-            if Userspace::get().is_root() {
-                super::time::time_set(args()[0] as u32);
+            let user = Userspace::get();
+            if !user.is_root() {
+                return Err(PosixError::EPERM);
             }
-            match Userspace::get().error {
-                Some(err) => Err(err),
-                None => Ok(0),
-            }
+            set_time(user.args[0]);
+            Ok(0)
         }
         number if sys::is_unimplemented(number) => Err(PosixError::ENOSYS),
         sys::FSTAT => trap_void(crate::fs::syscall_fstat),
@@ -245,26 +244,10 @@ fn handle_in_rust(number: usize) -> KResult<usize> {
             Ok(0)
         }
         sys::SLEEP => {
-            let _ctx = IrqGuard::disable_save();
-            let wake_time = get_time() + Userspace::get().args[0] as u32;
+            let sleep_time = Userspace::get().args[0];
+            let wake_time = get_time() + sleep_time;
 
-            loop {
-                let now = get_time();
-                let tout = time_tout();
-
-                if wake_time <= now {
-                    break;
-                }
-
-                if tout <= now || tout > wake_time {
-                    time_set_tout(wake_time);
-                }
-
-                Userspace::get()
-                    .proc()
-                    .sleep_user(time_tout_address(), PSLEP)?;
-            }
-
+            sleep_user_until(wake_time, PSLEP)?;
             Ok(0)
         }
         sys::SYNC => trap_void(crate::fs::syscall_sync),

src/interrupt/time.rs
@@ -1,16 +1,20 @@
+use core::sync::atomic::{AtomicUsize, Ordering};
+
 use crate::{
     interrupt::{interrupt::schedule_on_user_return, send_master_eoi, Registers},
     interrupt_entry,
     machine::{asm::enable_interrupts, TrapFrame},
-    proc::{ProcessManager, ProcessState},
+    proc::{Channel, ProcessManager, ProcessState},
+    serial::KResult,
+    sync::IrqGuard,
     user::Userspace,
 };
 
-const HZ: i32 = 60 * 2;
+const HZ: usize = 60;
 
-static mut LBOLT: i32 = 0;
-static mut TIME: u32 = 0;
-static mut TOUT: u32 = 0;
+static JIFFIES: AtomicUsize = AtomicUsize::new(0);
+static TIME: AtomicUsize = AtomicUsize::new(0);
+static TIMEOUT: AtomicUsize = AtomicUsize::new(0);
 
 #[no_mangle]
 pub extern "C" fn time_interrupt_body(_regs: *mut Registers, context: &mut TrapFrame) {
@@ -32,12 +36,24 @@ fn clock(context: &mut TrapFrame) {
         current.stat
     };
 
-    unsafe {
-        LBOLT += 1;
-        if LBOLT < HZ {
-            send_master_eoi();
-            return;
-        }
+    // fetch_add returns the old value, add 1 to get current jiffies.
+    let jiffies = JIFFIES.fetch_add(1, Ordering::Release) + 1;
+
+    if jiffies < HZ {
+        send_master_eoi();
+        return;
+    }
+
+    JIFFIES.store(0, Ordering::Release);
+    let time = TIME.fetch_add(1, Ordering::AcqRel) + 1;
+
+    #[cfg(feature = "debug_timer")]
+    crate::println_debug!("Time={}", time);
+
+    if time == TIMEOUT.load(Ordering::Acquire) {
+        #[cfg(feature = "debug_timer")]
+        crate::println_debug!("Waking up all sleepers");
+        ProcessManager::get().wakeup_all((&TIMEOUT).channel_addr());
     }
 
     if current_status == ProcessState::SRUN && !context.is_user() {
@@ -45,17 +61,8 @@ fn clock(context: &mut TrapFrame) {
         return;
     }
 
-    unsafe {
-        LBOLT -= HZ;
-        TIME += 1;
-    }
-
     enable_interrupts();
     send_master_eoi();
-
-    if get_time() == time_tout() {
-        ProcessManager::get().wakeup_all(time_tout_address());
-    }
 
     ProcessManager::get().recalc_pri();
 
@@ -64,46 +71,63 @@ fn clock(context: &mut TrapFrame) {
         ProcessManager::get().wakeup_runin();
     }
 
-    if context.is_user() {
-        let current = Userspace::get().proc();
-        if current.should_process() {
-            current.process_signal(context);
-        }
-        current.set_pri();
+    // Don't preempt in kernel space.
+    if !context.is_user() {
+        return;
     }
-}
 
-pub fn set_time(value: u32) {
-    unsafe {
-        TIME = value;
+    let current = Userspace::get().proc();
+    if current.should_process() {
+        current.process_signal(context);
     }
+    current.set_pri();
 }
 
-#[no_mangle]
-pub extern "C" fn get_time() -> u32 {
-    unsafe { TIME }
+pub fn set_time(value: usize) {
+    TIME.store(value, Ordering::Release);
 }
 
-#[no_mangle]
-pub extern "C" fn time_set(value: u32) {
-    set_time(value);
+pub fn get_time() -> usize {
+    TIME.load(Ordering::Acquire)
 }
 
-#[no_mangle]
-pub extern "C" fn time_tout() -> u32 {
-    unsafe { TOUT }
-}
+/// Set a timer that should wake us up at or earlier than `wake`.
+///
+/// # Note
+/// Call this function with IRQ disabled, otherwise we may have lost wakeups.
+///
+/// # Returns
+/// - `true` if the timer is set.
+/// - `false` if the timer has already expired.
+fn set_timer(wake: usize) -> bool {
+    let now = get_time();
+    let cur_timeout = TIMEOUT.load(Ordering::Acquire);
 
-#[no_mangle]
-pub extern "C" fn time_set_tout(value: u32) {
-    unsafe {
-        TOUT = value;
+    if now >= wake {
+        return false;
     }
+
+    // Keep the timeout if it's earlier than requested
+    if cur_timeout <= now || cur_timeout > wake {
+        TIMEOUT.store(wake, Ordering::Release);
+    }
+
+    true
 }
 
-#[no_mangle]
-pub extern "C" fn time_tout_address() -> usize {
-    core::ptr::addr_of!(TOUT) as usize
+pub fn sleep_user_until(wake: usize, pri: u32) -> KResult<()> {
+    let _irq = IrqGuard::disable_save();
+
+    while set_timer(wake) {
+        #[cfg(feature = "debug_timer")]
+        crate::println_debug!("Sleeping until {}", wake);
+
+        Userspace::get()
+            .proc()
+            .sleep_user((&TIMEOUT).channel_addr(), pri)?;
+    }
+
+    Ok(())
 }
 
 interrupt_entry!(TimeInterruptEntrance, time_interrupt_body);

src/kernel/init.rs
@@ -1,4 +1,4 @@
-use core::{arch::naked_asm, mem::MaybeUninit, ptr, sync::atomic::compiler_fence};
+use core::{arch::naked_asm, ptr, sync::atomic::compiler_fence};
 
 use crate::{
     compat::compat_flush_page_directory,
@@ -8,9 +8,7 @@ use crate::{
     kernel::kernel::rust_kernel_initialize,
     machine::{
         asm::enable_interrupts,
-        chip::{
-            cmos_read_byte_high, cmos_read_byte_low, cmos_read_time, init_peripherals, SystemTime,
-        },
+        chip::{cmos_read_byte_high, cmos_read_byte_low, cmos_read_time, init_peripherals},
         enable_page_protection, init_gdt, init_idt, init_page_directory, init_user_page_table,
         load_gdt, load_idt,
     },
@@ -22,7 +20,7 @@ use crate::{
     vesa::{vesa_clear, vesa_init, VbeModeInfo},
 };
 
-use super::{diagnose::_diagnose_trace_on, syscall::_lib_open, utility::_make_kernel_time};
+use super::{diagnose::_diagnose_trace_on, syscall::_lib_open};
 
 const KERNEL_SPACE_START_ADDRESS: usize = 0xc0000000;
 const VESA_MODE_INFO_ADDR: usize = KERNEL_SPACE_START_ADDRESS + 0x7e00;
@@ -142,9 +140,8 @@ fn init_vesa() {
 }
 
 fn init_kernel_time() {
-    let mut time = MaybeUninit::<SystemTime>::uninit();
-    cmos_read_time(time.as_mut_ptr());
-    set_time(_make_kernel_time(time.as_ptr()));
+    let time = cmos_read_time();
+    set_time(time.to_kernel_time());
 }
 
 fn read_memory_size() {

src/kernel/utility.rs
@@ -8,11 +8,10 @@ use crate::serial::KResult;
 use crate::sync::IrqGuard;
 use crate::user::Userspace;
 
-const SECONDS_IN_MINUTE: u32 = 60;
-const SECONDS_IN_HOUR: u32 = 3600;
-const SECONDS_IN_DAY: u32 = 86400;
-const DAYS_BEFORE_MONTH: [u32; 13] = [
-    u32::MAX,
+const SECONDS_IN_MINUTE: usize = 60;
+const SECONDS_IN_HOUR: usize = 3600;
+const SECONDS_IN_DAY: usize = 86400;
+const DAYS_BEFORE_MONTH: [usize; 12] = [
     0,
     31,
     59,
@@ -27,118 +26,36 @@ const DAYS_BEFORE_MONTH: [u32; 13] = [
     334,
 ];
 
-#[no_mangle]
-pub extern "C" fn _mem_copy(src: usize, dst: usize, count: u32) {
-    let src = src as *const u8;
-    let dst = dst as *mut u8;
+impl SystemTime {
+    pub fn to_kernel_time(&self) -> usize {
+        let current_year = 2000 + self.year;
 
-    for offset in 0..count as usize {
-        unsafe {
-            dst.add(offset).write(src.add(offset).read());
+        let mut seconds = self.second as usize;
+        seconds += self.minute as usize * SECONDS_IN_MINUTE;
+        seconds += self.hour as usize * SECONDS_IN_HOUR;
+
+        let mut days = (self.day_of_month - 1) as usize;
+
+        assert!(self.month > 0 && self.month < 13, "Invalid month");
+        days += DAYS_BEFORE_MONTH[self.month as usize - 1];
+        if is_leap(current_year) && self.month >= 3 {
+            days += 1;
         }
-    }
-}
 
-#[no_mangle]
-pub extern "C" fn _calculate_page_need(memory_need: u32, page_size: u32) -> i32 {
-    let mut page_required = memory_need / page_size;
-    if memory_need % page_size != 0 {
-        page_required += 1;
-    }
-    page_required as i32
-}
-
-#[no_mangle]
-pub extern "C" fn _get_major(dev: i16) -> i16 {
-    dev >> 8
-}
-
-#[no_mangle]
-pub extern "C" fn _get_minor(dev: i16) -> i16 {
-    dev & 0x00ff
-}
-
-#[no_mangle]
-pub extern "C" fn _set_major(dev: i16, value: i16) -> i16 {
-    (dev & 0x00ff) | (value << 8)
-}
-
-#[no_mangle]
-pub extern "C" fn _set_minor(dev: i16, value: i16) -> i16 {
-    (dev & !0x00ff) | (value & 0x00ff)
-}
-
-#[no_mangle]
-pub extern "C" fn _dword_copy(src: *const i32, dst: *mut i32, count: i32) {
-    for offset in 0..count.max(0) as usize {
-        unsafe {
-            dst.add(offset).write(src.add(offset).read());
+        for year in 1970..current_year {
+            days += days_in_year(year);
         }
+
+        seconds + days * SECONDS_IN_DAY
     }
 }
 
-#[no_mangle]
-pub extern "C" fn _min(a: i32, b: i32) -> i32 {
-    if a < b {
-        a
-    } else {
-        b
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn _max(a: i32, b: i32) -> i32 {
-    if a > b {
-        a
-    } else {
-        b
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn _bcd_to_binary(value: i32) -> i32 {
-    ((value >> 4) * 10) + (value & 0x0f)
-}
-
-#[no_mangle]
-pub extern "C" fn _io_move(src: *const u8, dst: *mut u8, count: i32) {
-    for offset in 0..count.max(0) as usize {
-        unsafe {
-            dst.add(offset).write(src.add(offset).read());
-        }
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn _make_kernel_time(time: *const SystemTime) -> u32 {
-    let time = unsafe { time.as_ref().expect("_make_kernel_time null time") };
-    let current_year = 2000 + time.year;
-
-    let mut seconds = time.second as u32;
-    seconds += time.minute as u32 * SECONDS_IN_MINUTE;
-    seconds += time.hour as u32 * SECONDS_IN_HOUR;
-
-    let mut days = (time.day_of_month - 1) as u32;
-    days += DAYS_BEFORE_MONTH[time.month as usize];
-    if _is_leap_year(current_year) && time.month >= 3 {
-        days += 1;
-    }
-
-    for year in 1970..current_year {
-        days += _days_in_year(year);
-    }
-
-    seconds + days * SECONDS_IN_DAY
-}
-
-#[no_mangle]
-pub extern "C" fn _is_leap_year(year: i32) -> bool {
+fn is_leap(year: u32) -> bool {
     year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
 
-#[no_mangle]
-pub extern "C" fn _days_in_year(year: i32) -> u32 {
-    if _is_leap_year(year) {
+fn days_in_year(year: u32) -> usize {
+    if is_leap(year) {
         366
     } else {
         365

src/machine/chip.rs
@@ -3,15 +3,15 @@ use crate::{
     sync::IrqGuard,
 };
 
-#[repr(C)]
+#[derive(Debug, Clone, Copy, Default)]
 pub struct SystemTime {
-    pub second: i32,
-    pub minute: i32,
-    pub hour: i32,
-    pub day_of_month: i32,
-    pub month: i32,
-    pub year: i32,
-    pub day_of_week: i32,
+    pub second: u32,
+    pub minute: u32,
+    pub hour: u32,
+    pub day_of_month: u32,
+    pub month: u32,
+    pub year: u32,
+    pub day_of_week: u32,
 }
 
 const PIT_INPUT_FREQ: i32 = 1_193_180;
@@ -40,7 +40,7 @@ const RTC_DAY_OF_MONTH: u8 = 0x07;
 const RTC_MONTH: u8 = 0x08;
 const RTC_YEAR: u8 = 0x09;
 const RTC_STATUS_REGISTER_A: u8 = 0x0a;
-const RTC_UPDATE_IN_PROGRESS: i32 = 0x80;
+const RTC_UPDATE_IN_PROGRESS: u32 = 0x80;
 
 const EXTENDED_MEMORY_ABOVE_1MB_LOW: u8 = 0x30; /* 1MB以上扩展内存(低字节) */
 const EXTENDED_MEMORY_ABOVE_1MB_HIGH: u8 = 0x31;
@@ -113,41 +113,39 @@ fn chip8259a_irq_disable(irq: u32) {
     }
 }
 
-pub fn cmos_read_time(time: *mut SystemTime) {
-    if time.is_null() {
-        return;
-    }
+pub fn cmos_read_time() -> SystemTime {
+    let mut time = SystemTime::default();
 
     while cmos_read_byte(RTC_STATUS_REGISTER_A) & RTC_UPDATE_IN_PROGRESS != 0 {}
 
-    unsafe {
-        (*time).second = bcd_to_binary(cmos_read_byte(RTC_SECONDS));
-        (*time).minute = bcd_to_binary(cmos_read_byte(RTC_MINUTES));
-        (*time).hour = bcd_to_binary(cmos_read_byte(RTC_HOURS));
-        (*time).day_of_month = bcd_to_binary(cmos_read_byte(RTC_DAY_OF_MONTH));
-        (*time).month = bcd_to_binary(cmos_read_byte(RTC_MONTH));
-        (*time).year = bcd_to_binary(cmos_read_byte(RTC_YEAR));
-        (*time).day_of_week = bcd_to_binary(cmos_read_byte(RTC_DAY_OF_WEEK));
-    }
+    time.second = bcd_to_binary(cmos_read_byte(RTC_SECONDS));
+    time.minute = bcd_to_binary(cmos_read_byte(RTC_MINUTES));
+    time.hour = bcd_to_binary(cmos_read_byte(RTC_HOURS));
+    time.day_of_month = bcd_to_binary(cmos_read_byte(RTC_DAY_OF_MONTH));
+    time.month = bcd_to_binary(cmos_read_byte(RTC_MONTH));
+    time.year = bcd_to_binary(cmos_read_byte(RTC_YEAR));
+    time.day_of_week = bcd_to_binary(cmos_read_byte(RTC_DAY_OF_WEEK));
+
+    time
 }
 
-fn cmos_read_byte(cmos_offset: u8) -> i32 {
+fn cmos_read_byte(cmos_offset: u8) -> u32 {
     unsafe {
-        let ctx = IrqGuard::disable_save();
+        let _ctx = IrqGuard::disable_save();
         IOPort::out_byte(CMOS_ADDR_PORT, cmos_offset);
-        IOPort::in_byte(CMOS_DATA_PORT) as i32
+        IOPort::in_byte(CMOS_DATA_PORT) as u32
     }
 }
 
-pub fn cmos_read_byte_low() -> i32 {
+pub fn cmos_read_byte_low() -> u32 {
     cmos_read_byte(EXTENDED_MEMORY_ABOVE_1MB_LOW)
 }
 
-pub fn cmos_read_byte_high() -> i32 {
+pub fn cmos_read_byte_high() -> u32 {
     cmos_read_byte(EXTENDED_MEMORY_ABOVE_1MB_HIGH)
 }
 
-fn bcd_to_binary(value: i32) -> i32 {
+fn bcd_to_binary(value: u32) -> u32 {
     (value & 0x0f) + ((value >> 4) * 10)
 }
 